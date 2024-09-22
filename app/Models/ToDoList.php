60854185785019719c861a5f68d035a8a449ebe3
Moji <472306m@gmail.com>
2024-09-22 09:58:59 +0330

    List Relation

--- app/Models/ToDoList.php
+++ app/Models/ToDoList.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class ToDoList extends Model
 {
     use HasFactory;
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function task()
+    {
+        return $this->hasMany(Task::class);
+    }
 }

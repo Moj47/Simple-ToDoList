02c162343fbcab56761cfcf0add6d5f038818b11
Moji <472306m@gmail.com>
2024-09-23 10:52:42 +0330

    Test for store and delete list

--- tests/Feature/ListTest.php
+++ tests/Feature/ListTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\ToDoList;
 use Tests\TestCase;
 use App\Models\User;
 use Database\Seeders\DatabaseSeeder;
@@ -24,10 +25,30 @@ class ListTest extends TestCase
     {
         $seeder=new DatabaseSeeder();
         $seeder->run();
-        $user=User::findOrFail(11);
-        $list=User::findOrFail(11)->toDoLists()->first();
-        $this->actingAs($user)->get(route('lists.show',$list))
+        $user = User::inRandomOrder()->first();
+        $list=$user->toDoLists()->inRandomOrder()->first();
+        $this->actingAs($user)->get(route('lists.show', ['toDoList' => $list->id]))
         ->assertSuccessful()
         ->assertViewHas('list');
+    }
+    public function testMakeAListWithStoreMethod()
+    {
+        $user=User::factory()->create();
+        $list=ToDoList::factory()->make(['user_id'=>$user->id]);
+        $this->actingAs($user)
+        ->post(route('lists.store',$list->toArray()))
+        ->assertRedirect();
+
+        $this->assertDatabaseHas('to_do_lists',$list->toArray());
+    }
+    public function testDeleteAListWitDestroyMethod()
+    {
+        $user=User::factory()->create();
+        $list=ToDoList::factory()->create(['user_id'=>$user->id]);
+        $this->assertDatabaseHas('to_do_lists',$list->toArray());
+        $this->actingAs($user)
+        ->delete(route(name: 'lists.destroy',  parameters: $list))
+        ->assertRedirect();
+        $this->assertDatabaseMissing('to_do_lists',$list->toArray());
     }
 }
